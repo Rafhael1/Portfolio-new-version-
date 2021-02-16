2910dc2d5dbfb4bd3e20bda23ae3333a2751d660
Rafhael <rafamlf.2011@gmail.com>
2021-02-16 13:04:11 -0300

    postgresql

--- components/Cards/Technologies.js
+++ components/Cards/Technologies.js
@@ -32,6 +32,7 @@ export default function Technologies() {
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1200px-Node.js_logo.svg.png" style={{width: "200px", height: "150px"}}/>
     <img src="https://buttercms.com/static/images/tech_banners/ExpressJS.png" style={{width: "280px", height: "90px"}} />
     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Nextjs-logo.svg/800px-Nextjs-logo.svg.png" style={{width: "250px", height: "130px"}} />
+    <img src="https://d1.awsstatic.com/rdsImages/postgresql_logo.6de4615badd99412268bc6aa8fc958a0f403dd41.png" style={{width: "200px", height: "200px"}} />
 
         </div>
     )
